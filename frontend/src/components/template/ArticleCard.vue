23f098c42b40cf23c18879c39c8f61b343717418
matheusgzancanela <matheusgzpessoal@gmail.com>
2020-11-20 15:18:30 -0300

    mudei a pagina home

--- frontend/src/components/template/ArticleCard.vue
+++ frontend/src/components/template/ArticleCard.vue
@@ -1,23 +1,20 @@
 
 <template>
-    <div class="article-card-container">
-        <div class="article-card-image-container">
-            <router-link :to="{ name: 'articleById', params: { id: article.id }}">
-                <div class="image-container" :style="{backgroundImage: 'url('+ article.imageUrl +')'}">
-                    <div class="article-title">
-                        <h1>{{ article.name }}</h1>
-                    </div>
-                    <div class="after">
-                        <h1>{{ article.description }}</h1>
-                    </div>
-                </div>
-            </router-link>
-        </div>
-    </div>
+    <div class="news">
+        <router-link :to="{ name: 'articleById', params: { id: article.id }}">
+            <figure class="article">
+                <img v-if="article.imageUrl" :src="article.imageUrl" />
+                <img v-else src="@/assets/noticia.png" />
+                <figcaption>
+                    <h3>{{ article.name }}</h3>
+                    <p>{{ article.description }}</p>
+                </figcaption>
+            </figure>
+        </router-link>
+	</div>
 </template>
 
 <script>
-
 export default {
     name: 'ArticleCard',
     components: { },
@@ -28,65 +25,101 @@ export default {
       }
     }
 }
-
 </script>
 
-
 <style>
-
-.filter {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background:rgba(0, 0, 0, 0.39);
-    z-index: 0;
+.news .article figcaption {
+  position: absolute;
 }
+.rel,
 
-.article-card-image-container {
-    height: 100%;
-    width: 100%;
-    box-shadow: 0 5px 20px rgba(0, 0, 0, 0.5);
-    border-radius: 5px;
+.news .article {
+  position: relative;
+  box-shadow: inset 0 0 1em black, 0 0 1em black;
+  border: rgba(0,0,0,0.2);
+  border-radius: 10px;
 }
-
-.article-title {
-    padding: 25px;
-    font-weight: 800;
-    font-size: 35px;
-    color: white;
-    z-index: 1;
+.fix {
+  position: fixed;
 }
-
-.article-card-container{
-    padding: 5px;
-    width: 100%;
-    height: 100%;
-    margin: 5px;
-    height: 250px;
+.dfix {
+  display: inline;
 }
-
-.image-container {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    background-size: cover;
-    background-position: initial;
+.dib {
+  display: inline-block;
 }
-
-.image-container .after {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    display: none;
-    color: white;
+.db {
+  display: block;
 }
-
-.image-container:hover .after {
-    display: block;
-    background: rgba(0, 0, 0, 0.7);
+.dn {
+  display: none;
 }
-
+.df,
+.news {
+  display: flex;
+}
+.dif {
+  display: inline-flex;
+}
+.dg {
+  display: grid;
+}
+.dig {
+  display: inline-grid;
+}
+.vm,
+body {
+  background: #24282f;
+  font-family: 'Alegreya Sans';
+}
+.wrapper {
+  padding: 15px;
+}
+.news {
+  flex-flow: row wrap;
+}
+.news .article {
+  overflow: hidden;
+  width: 600px;
+  height: 300px;
+  margin: 50px;
+}
+.news .article img {
+  width: 100%;
+  min-height: 100%;
+  transition: 0.2s;
+}
+.news .article figcaption {
+  font-size: 14px;
+  text-shadow: 0 1px 0 rgba(51,51,51,0.3);
+  color: #fff;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  padding: 40px;
+  box-shadow: 0 0 2px rgba(0,0,0,0.2);
+  background: rgba(6,18,53,0.6);
+  opacity: 0;
+  transform: scale(1.15);
+  transition: 0.2s;
+}
+.news .article figcaption h3 {
+  color: #3792e3;
+  font-size: 16px;
+  margin-bottom: 0;
+  font-weight: bold;
+}
+.news .article:hover img,
+.news .article:focus img,
+.news .article:active img {
+  filter: blur(3px);
+  transform: scale(0.97);
+}
+.news .article:hover figcaption,
+.news .article:focus figcaption,
+.news .article:active figcaption {
+  opacity: 1;
+  transform: none;
+}
 </style>
-
